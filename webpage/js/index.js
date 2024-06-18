a142b2b02439b2dbb23038c245bcf384ac8030ba
Nick Mensah <103342287+mensahTribeWeb@users.noreply.github.com>
2024-06-18 02:19:51 -0500

    Update index.js

--- webpage/js/index.js
+++ webpage/js/index.js
@@ -27,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+
 // var idx = Math.floor(new Date().getHours());
 // var body = document.getElementsByTagName("body")[0];
 // body.className = "heaven-" + idx;
